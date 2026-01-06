c3d81e18821317ae21bebdaccb05cde783c8b750
Alessio Deiana <adeiana@gmail.com>
2026-01-06 03:36:35 -0600

    Add example for creating test items

---
+++ examples/add_test_items.rs
@@ -0,0 +1,27 @@
+//! Add test items to vault
+//! Run with: cargo run --example add_test_items
+
+use enpass_vault::Vault;
+
+fn main() {
+    let vault_path = "/home/osso/Documents/Enpass/Vaults/87d16630-baa2-4ac3-a591-102d0d5e43b4";
+    let password = "none";
+
+    let vault = Vault::open(vault_path, password).expect("Failed to open vault");
+
+    for i in 1..=10 {
+        let title = format!("Test Item {:02}", i);
+        let fields = [
+            ("username", &format!("user{}", i) as &str, false),
+            ("password", &format!("pass{}", i) as &str, true),
+            ("url", &format!("https://test{}.example.com", i) as &str, false),
+        ];
+
+        match vault.create_item(&title, "login", &fields) {
+            Ok(uuid) => println!("Created: {} ({})", title, uuid),
+            Err(e) => eprintln!("Failed to create {}: {}", title, e),
+        }
+    }
+
+    println!("Done!");
+}
